3256382a5ec514ac40f8c09b16c39fa265a8c1c5
whoistim <whoistim@sbcglobal.net>
2014-12-03 09:19:44 -0800

    finished Angular lab

--- app/books_controller.js
+++ app/books_controller.js
@@ -3,7 +3,22 @@ var bookly = angular.module("booklyApp", []);
 bookly.controller('BooksController', function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
+  $scope.sort = "author";
+  $scope.total=0;
+
 
   //Code to manage cart goes here
+  $scope.cart = [];
+  $scope.addToCart = function(book){
+    $scope.cart.push(book);
+    console.log($scope.cart);
+    $scope.total = $scope.total + book.price;
+  };
+
+  $scope.dumpTheCart = function() {
+    $scope.cart =[];
+    $scope.total = 0;
+  };
+
 
 });
